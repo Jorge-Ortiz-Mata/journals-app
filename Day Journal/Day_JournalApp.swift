bdb06a4b34279cac315aababe5978ab32d90f13c
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2024-01-11 09:47:12 -0600

    Initial Commit

---
+++ Day Journal/Day_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day_JournalApp.swift
+//  Day Journal
+//
+//  Created by Jorge on 11/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Day_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
